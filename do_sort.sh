418e9646db0f80b9c59ff1431891ddc0587e5bd3
Shawn Klein <sklein8@uic.edu>
2022-02-02 21:31:26 -0600

    fix typo org to orig

--- do_sort.sh
+++ do_sort.sh
@@ -3,7 +3,7 @@ do
   sort $filename | diff $filename - &>/dev/null
   if [ $? -eq 1 ]
   then
-    cp $filename ${filename%.txt}.org.txt
+    cp $filename ${filename%.txt}.orig.txt
     sort $filename -o $filename 
   fi
 done
